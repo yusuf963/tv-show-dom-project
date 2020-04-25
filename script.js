29e5701f2171870373a97c3198b91ba4287b666a
yusuf963 <58535314+yusuf963@users.noreply.github.com>
2020-04-25 18:20:39 +0100

    100-done

--- script.js
+++ script.js
@@ -4,19 +4,50 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function makeSingleTVShowListing(episode) {
+  const episodeDiv =  document.createElement("div")
+  let ban = document.createElement("h2")
+  ban.innerText = ` ${episode.name} - S${addingZero(episode.season)}E${addingZero(episode.number)}`;
+  let image = document.createElement("IMG")
+   episodeDiv.className = "episode"
+   episodeDiv.appendChild(ban)
+  image.src = episode.image.medium
+  episodeDiv.appendChild(image)
+  let plot = document.createElement("div")
+  plot.innerHTML = episode.summary
+  episodeDiv.appendChild(plot)
+  return episodeDiv
+}
+
+ function addingZero (num){
+   if (num<10){
+     return `0${num}`
+   }else{ 
+     return num.toString()
+   }
+ }
+
+
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.name} episode(s)`;
+
+
+  // create episode div
+  //let episodeDiv = makeSingleTVShowListing(episodeList[2]);
+ 
+ // add episode div to root elem
+  //rootElem.appendChild(episodeDiv)
+ 
+  episodeList.forEach(episode => {
+    let episodeDiv = makeSingleTVShowListing(episode);
+    rootElem.appendChild(episodeDiv)
+    
+  });
+
+
+  
   console.log(episodeList)
 }
 
 window.onload = setup;
-
-
-
-function adding (){
-  a = 1;
-  b = 3;
-  return a + b
-}
-console.log(adding())
